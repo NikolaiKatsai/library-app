2fd1a4f593e0e86817c00f4903437d36c288c49e
NikolayKatsai <alaska.rok123@gmail.com>
2019-10-23 12:03:25 +0300

    Pr-30 finished

--- src/main/java/mate/academy/spring/dao/impl/UserDaoImp.java
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImp.java
@@ -1,9 +1,10 @@
-package mate.academy.spring.dao;
+package mate.academy.spring.dao.impl;
 
 import java.util.List;
 
 import javax.persistence.TypedQuery;
 
+import mate.academy.spring.dao.UserDao;
 import mate.academy.spring.entity.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,8 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public List<User> listUsers() {
-        @SuppressWarnings("unchecked")
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery("from User", User.class);
         return query.getResultList();
     }
 }
