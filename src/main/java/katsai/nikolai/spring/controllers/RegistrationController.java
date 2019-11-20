ae03719c2789052955c2149698333d6e77039599
NikolaiKatsai <36364763+NikolaiKatsai@users.noreply.github.com>
2019-11-20 12:18:54 +0200

    Spring security branch (#6)
    * Pr-34-35 redone
    
    * Hibernate dialect added
    
    * User column name changed
    
    * UserDtoValidation done

--- src/main/java/katsai/nikolai/spring/controllers/RegistrationController.java
+++ src/main/java/katsai/nikolai/spring/controllers/RegistrationController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -25,10 +24,10 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String register(@ModelAttribute("userDto") @Valid UserRegistrationDto userDto,
+    public String register(@Valid UserRegistrationDto userDto,
                            BindingResult result, Model model) {
         if (result.hasErrors()) {
-            model.addAttribute("errorMsg", "registration failed");
+            model.addAttribute("errorMsg", "Registration failed: passwords do not match or invalid email");
             return registrationPage(model);
         }
         userService.registerNewUserAccount(userDto);
